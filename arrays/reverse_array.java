f38f9bd9cf8b94a575fe9240721d48dcc01dc741
yashwanth <yashwanth.d52917@gmail.com>
2026-05-30 16:23:17 +0530

    commit

---
+++ arrays/reverse_array.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class reverse_array {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6};
+        int temp;
+        int start=0;
+        int end=arr.length-1;
+        while(start<end)
+        {
+            temp =arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+
+            start++;
+            end--;
+        }
+        for(int num:arr)
+        {
+            System.out.print(" "+num);
+        }
+
+    }
+}
