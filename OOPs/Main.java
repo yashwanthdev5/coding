569f8225cad2f87f31c2edb419d8e2312a7d0c3e
yashwanth <yashwanth.d52917@gmail.com>
2026-06-01 18:34:07 +0530

    clearly learned polymorphism and casting

---
+++ OOPs/Main.java
@@ -0,0 +1,16 @@
+package OOPs;
+
+// example of Downcasting .....
+class Parent {
+    void show() { System.out.println("Parent"); }
+}
+class Child extends Parent {
+    void show() { System.out.println("Child"); }
+}
+public class Main {
+    public static void main(String[] args) {
+        Parent p = new Child();
+        ((Child) p).show();
+    }
+}
+
---
+++ OOPs/RunTimePolymorphism.java
@@ -0,0 +1,33 @@
+package OOPs;
+
+class Animal{
+      void sound()
+      {
+        System.out.println("Animal sound");
+      }
+      void bark()
+       {
+        System.out.println("bark");
+       }
+}
+class Dog extends Animal
+{
+     // method Overriding..
+       void sound()
+       {
+        System.out.println("Dog sound");
+       }
+       // gives compilation error if only present in this class...
+    //     void bark()
+    //    {
+    //     System.out.println("bark");
+    //    }
+       
+}
+public class RunTimePolymorphism {
+    public static void main(String[] args) {
+        Animal a=new Dog();
+        a.sound();
+        a.bark();
+    }
+}
