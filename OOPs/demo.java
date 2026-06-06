9af15e4747bc98cb0c72bcfecdc8cf3f89637ba1
yashwanth <yashwanth.d52917@gmail.com>
2026-06-06 18:17:16 +0530

    demo of the exception

---
+++ OOPs/demo.java
@@ -0,0 +1,12 @@
+package OOPs;
+
+public class demo {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+    }
+
+}
