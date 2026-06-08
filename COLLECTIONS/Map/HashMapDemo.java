cc9b0043061d7c8ad836c66019cf13ccbeb7eb1b
yashwanth <yashwanth.d52917@gmail.com>
2026-06-08 19:32:31 +0530

    learned HashMap

---
+++ COLLECTIONS/Map/HashMapDemo.java
@@ -0,0 +1,27 @@
+package Map;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        HashMap<Integer, String> h = new HashMap<>();
+        h.put(1, "arun");
+        h.put(2, "yashwanth");
+        h.put(3, "sai");
+        h.put(4, "supriya");
+        for (int i = 1; i < 5; i++) {
+            System.out.println(h.get(i));
+        }
+        System.out.println(h.isEmpty());
+        System.out.println(h.remove(2));
+        System.out.println(h.size());
+        System.out.println(h.containsKey(2));
+        HashMap<String, Integer> map = new HashMap<>();
+        // here the value will be updated to 100
+        map.put("Java", 90);
+        map.put("Java", 100);
+
+        System.out.println(map.get("Java"));
+
+    }
+}
