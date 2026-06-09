45962623dc43d1565ceb0dfab22c3738e43628fd
yashwanth <yashwanth.d52917@gmail.com>
2026-06-09 20:05:06 +0530

    Completed Linear search

---
+++ DSA/Arrays_DSA/LinearSearch.java
@@ -0,0 +1,46 @@
+package DSA.Arrays_DSA;
+
+import java.util.Scanner;
+
+class Linear{
+    public static int linearsearch(int[] arr,int target)
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr.length==0||arr==null)
+            {
+               return -1;
+            }
+            if(target==arr[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Array size :");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter "+size+" elements :");
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter the Element you want to search :");
+        int target=sc.nextInt();
+        int res=Linear.linearsearch(arr,target);
+        if(res>-1){
+            System.out.println("The element found at Index : "+res);
+        }else
+        {
+             System.out.println("The element not found");
+        }
+        
+      sc.close();
+    }
+}
